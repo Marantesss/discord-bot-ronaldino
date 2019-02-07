da13c8ed079a080bb13a71ac0f175bedcc414d48
Gonçalo Marantes <up201706917@fe.up.pt>
2019-02-07 12:54:14 +0000

    Minor changes in bot.py

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### discord.py
 ```
-python3 -m pip install -U https://github.com/Rapptz/discord.py/archive/async.zip#egg=discord.py\[voice\]
+python3 -m pip install -U https://github.com/Rapptz/discord.py/archive/async.zip#egg=discord.py[voice]
 ```
 
 ### websockets
--- bot.py
+++ bot.py
@@ -10,10 +10,11 @@ token = "NTQyMDY2NjEwODc4MjgzODAw.Dzorpw.3RohsSVdbsKywlK4XXF2UOq2Yqg"
 @client.event
 async def on_ready():
     try:
-        # print bot infomration
+        # print bot information
         print(client.user.name)
         print(client.user.id)
         print("Discord.py Version: {}".format(discord.__version__))
+        await client.change_presence(game=discord.Game(name="Under construction"))
         
     except Exception as e:
         print(e)
@@ -23,11 +24,18 @@ async def on_ready():
 async def on_message(message):
 	# print message content
 	# await message.content
-    # we do not want the bot to respond to itslef
+    # we do not want the bot to respond to itself
     if message.author == client.user:
         return
+    # Hello
     if message.content.startswith("+hello"):
         msg = "Hello {0.author.mention}".format(message)
+        await client.send_message(message.channel, msg)
+    # Information
+    if message.content.startswith("+info"):
+        msg = "Thank you {0.author.mention} for wanting to know more about me :yum:\n".format(message)
+        msg += "I am currently being built with much love, but I still have a long way to go!\n"
+        msg += "If you happen to come accross any bugs or want a new feature to be implemented, please do say so! :desktop:"
         await client.send_message(message.channel, msg)
 
 # start bot
